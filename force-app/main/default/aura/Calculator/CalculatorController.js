05147cead8a38cab195cd2aec5f3ce56860d833f
Gopi Koka <gopikrishna416@gmail.com>
2021-04-23 19:07:49 +0100

    Calculator Updates

--- force-app/main/default/aura/Calculator/CalculatorController.js
+++ force-app/main/default/aura/Calculator/CalculatorController.js
@@ -3,6 +3,6 @@
         var num1 = component.get("v.num1");
         var num2 = component.get("v.num2");
         var add = num1 + num2 ; 
-        component.set("v.output");
+        component.set("v.output",add);
     }
 })
