f5b5fed83d752f5b7ce509d89c3dad5842b4fba5
Gopi Koka <gopikrishna416@gmail.com>
2021-04-26 10:12:32 +0100

    CustomObjects

--- force-app/main/default/lwc/calculator1/calculator1.js
+++ force-app/main/default/lwc/calculator1/calculator1.js
@@ -7,6 +7,7 @@ export default class Calculator extends LightningElement {
     add(){
         console.log(this.template.querySelector('.num1').value);
         console.log(this.template.querySelector('.num2').value);
+
         this.num1 = this.template.querySelector('.num1').value;
         this.num2 = this.template.querySelector('.num2').value;
         this.output = parseInt(this.num2) + parseInt(this.num2); 
